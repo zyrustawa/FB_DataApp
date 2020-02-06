27c51724edc7b54ee83d2332632240bf8ce0dd49
tawanda madzidzise <awandamadzidzise@gmail.com>
2020-02-06 15:20:31 +0200

    Added WorkPlace for community/ clinic  database table

---
+++ FB_DataApp/Database/Tables/WorkPlace.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace FB_DataApp.Database.Tables
+{
+    class WorkPlace
+    {
+        [PrimaryKey, AutoIncrement]
+        public string WorkPlaceID { get; set; }
+        public string WorkPlaceName { get; set; }
+    }
+}
