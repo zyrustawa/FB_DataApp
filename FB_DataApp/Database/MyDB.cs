587703bc449a2c02a2fb9fe95c2001f9a02377a4
tawanda madzidzise <awandamadzidzise@gmail.com>
2020-02-06 15:12:36 +0200

    Added folder Tables for all database tables Added MyDB.cs for database connection Added LayHealthWorker.cs for the LayHealthWorker table

---
+++ FB_DataApp/Database/MyDB.cs
@@ -0,0 +1,25 @@
+﻿using Android.Util;
+using SQLite;
+using System;
+using System.IO;
+
+namespace take.DBconn
+{
+    class MyDB
+    {
+        //private String DbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "Fbdb.db3");
+        public SQLiteConnection MyConn()
+        {
+            var dbpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "FbSessiondb.sqlite");
+            if(File.Exists(dbpath))
+            {
+                Log.Error("FBdatabase","file exists ");
+            }
+            else
+            {
+                Log.Error("FBdatabase", "file does not exist");
+            }
+            return new SQLiteConnection(dbpath, true);
+        }
+    }
+}
---
+++ FB_DataApp/Database/Tables/LayHealthWorker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace FB_DataApp.Database.Tables
+{
+    class LayHealthWorker
+    {
+       [PrimaryKey, AutoIncrement]
+        public String LHWid { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Contact { get; set; }
+        public String Address { get; set; }
+        public String Dob { get; set; }//dob=date of birth
+        public String Clinic { get; set; }
+        public String Gender { get; set; }
+        public String Password { get; set; }
+
+
+
+
+    }
+}
